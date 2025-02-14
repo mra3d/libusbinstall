5d66a74a4eb612646253fab4c7808d1dfcb50c24
mra3d <htc.medhat@gmail.com>
2025-02-14 23:59:05 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -6,7 +6,7 @@ function lineaSeparador()
 function clearConTexto()
 {
 	lineaSeparador
-	echo '[Legitunlocks]'
+	echo '[LegitUnlocks]'
 	lineaSeparador
 } 
 function linux_depends()
@@ -15,7 +15,8 @@ function linux_depends()
 	echo '[Linux]'
 	lineaSeparador
 	echo 'The installation for Linux dependencies needs sudo permission, please input the password of your user (it is invisible) and press return'
-	
+	lineaSeparador
+	echo 'La instalación para las dependencias de Linux necesita permiso sudo, por favor ingresa la contraseña de tu usuario (es invisible) y presiona enter'
 	lineaSeparador	
 	sudo -v
 	if [[ $(which apt-get) ]]
@@ -51,22 +52,45 @@ function linux_depends()
 		clearConTexto
 		python3 -m pip install --upgrade pip
 		clearConTexto
-		# Check for libprotobuf.so.23
+		# Comprobación para libprotobuf.so.23
 		if ! locate libprotobuf.so.23; then
-		    echo "libprotobuf.so.23 is not present, downloading and installing..."
+		    echo "libprotobuf.so.23 no está presente, descargando e instalando..."
+		    # Descargar el paquete .deb de libprotobuf23 desde Debian
 		    wget http://archive.ubuntu.com/ubuntu/pool/main/p/protobuf/libprotobuf23_3.12.4-1ubuntu7_amd64.deb
 		    if [[ -f "libprotobuf23_3.12.4-1ubuntu7_amd64.deb" ]]; then
-		        echo "Package libprotobuf23_3.12.4-1ubuntu7_amd64.deb downloaded successfully."
+		        echo "Paquete libprotobuf23_3.12.4-1ubuntu7_amd64.deb descargado exitosamente."
+		        # Instalar el paquete .deb
 		        sudo dpkg -i libprotobuf23_3.12.4-1ubuntu7_amd64.deb
+		        # Corregir dependencias faltantes si es necesario
 		        sudo apt --fix-broken install
 		        rm libprotobuf23_3.12.4-1ubuntu7_amd64.deb
 		    else
-		        echo "Error: Could not download the libprotobuf23 package."
+		        echo "Error: No se pudo descargar el paquete .deb de libprotobuf23."
 		        exit 1
 		    fi
 		else
-		    echo "libprotobuf.so.23 already exists in the system."
+		    echo "libprotobuf.so.23 ya existe en el sistema."
 		fi
+		#Solo instalar ideviceactivation si ideviceactivation no está presente
+		if ! ideviceactivation -v ; then
+			build_libimobiledevice
+		else
+			clearConTexto
+			echo '[iDevice]'
+			lineaSeparador
+			echo 'iDevice is already installed. Ok!'
+			lineaSeparador
+			echo 'iDevice ya está instalado. ¡Ok!'
+			lineaSeparador
+			sleep 2
+		fi
+	else
+		clearConTexto
+		echo "ERROR: PLEASE USE A DISTRO USING APT-GET SUCH AS UBUNTU"
+		lineaSeparador
+		echo "ERROR: USA UNA DISTRO QUE TENGA APT-GET COMO UBUNTU"
+		lineaSeparador
+		exit 1
 	fi
 }
 function macos_depends()
@@ -75,15 +99,21 @@ function macos_depends()
 	echo '[MacOS]'
 	lineaSeparador
 	echo 'The installation for MacOS dependencies needs sudo permission, please input the password of your user (it is invisible) and press return'
+	lineaSeparador
+	echo 'La instalación para las dependencias de MacOS necesita permiso sudo, por favor ingresa la contraseña de tu usuario (es invisible) y presiona enter'
 	lineaSeparador	
 	sudo -v
-	while true; do sudo -n true; sleep 60; kill -0 "$" || exit; done 2>/dev/null &
+	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 	if [[ ! -e $(which brew) ]] 
 	then
 		clearConTexto
 		echo '[Brew]'
 		lineaSeparador
 		echo 'The package manager brew will be installed.'
+		echo 'Please pay attention if the installation asks to press return (enter)'
+		lineaSeparador
+		echo 'Se instalará el administrador de paquetes brew.'
+		echo 'Presta atención si la instalación pide que presiones return (enter)'
 		lineaSeparador
 		sleep 5
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -93,22 +123,97 @@ function macos_depends()
 		lineaSeparador
 		echo 'brew is already installed. Ok!'
 		lineaSeparador
+		echo 'brew ya está instalado. ¡Ok!'
+		lineaSeparador
 		sleep 2
 	fi
 	clearConTexto
 	echo 'From this point the installation is automated and it may take 10-40 minutes depending on internet and CPU speed.'
+	echo 'So, grab a drink, sit back, relax and please wait.'
+	echo "You'll be unlocking devices very soon <3"
 	lineaSeparador
+	
 	sleep 5
 	clearConTexto
-
+	#brew doctor
+	#brew cleanup
+	#clearConTexto
+	#echo "Ignore the errors shown here..."
+	#lineaSeparador
+	#echo "Ignora los errores que se muestren..."
+	#lineaSeparador
+	
 	for i in {1..10}; do
 	    sudo chown -R $(whoami) /usr/local/share/man/man$i
 	done
 	
 	brew install pkg-config libtool automake cmake
+	# cmake puede que se instale como x86, elimina todo rastro de cmake y luego ejecuta 
+	# arch -arm64 /opt/homebrew/bin/brew install cmake
+	
+	#rm ~/.zprofile
+	#brew uninstall python-tk
+	#brew uninstall python-tk@3.9
+	#brew uninstall python-tk@3.10
+	#brew uninstall python
+	#brew uninstall python3
+	#brew uninstall python@3.7
+	#brew uninstall python@3.8
+	#brew uninstall python@3.9
+	#brew uninstall python@3.10
+	#clearConTexto
+	#brew install python@3.10
+	#brew link --force --overwrite python@3.10	
+	#brew install python-tk@3.10
+	#brew link --force --overwrite python-tk@3.10
 	clearConTexto
 
+	#Solo instalar ideviceactivation si ideviceactivation no está presente
+	if ! ideviceactivation -v ; then
+	
+		#	# Primero Openssl
+		#	
+		#	rm -rf openssl
+		#	git clone https://github.com/openssl/openssl.git
+		#	cd openssl
+		#	./config
+		#	make && sudo make install
+		#	cd ..
+		#	rm -rf openssl
+		#
+		#	# Despues libusb
+		#	
+		#	(obtener zip)
+		#	./configure
+		#	make
+		#	sudo make install
+		#	
+		#	# Despues libzip
+		#	
+		#	(obtener zip)
+		#	mkdir build
+		#	cd build
+		#	cmake ..
+		#	make
+		#	sudo make install
+		#	cd ..
+		
+		build_libimobiledevice
+	else
+		clearConTexto
+		echo '[iDevice]'
+		lineaSeparador
+		echo 'iDevice is already installed. Ok!'
+		lineaSeparador
+		echo 'iDevice ya está instalado. ¡Ok!'
+		lineaSeparador
+		sleep 2
+	fi
+	
+	# Instalar hasta el ultimo python por que instala openssl de brew
+	# Y rompe al compilado.
 	brew install python@3.13 python-tk@3.13
+	
 }
 function build_libimobiledevice()
 {
@@ -154,7 +259,7 @@ function build_libimobiledevice()
 	fi
 }
 clearConTexto
-echo 'Welcome! This script will install all the dependencies needed to run the tool without any problems or bugs.'
+echo 'Welcome! This script will install all the dependencies needed'
 lineaSeparador
 if [[ $(uname) == 'Linux' ]]
 then
@@ -164,6 +269,6 @@ then
 	macos_depends
 fi
 clearConTexto
-echo 'Dependencies installation is complete! You may now proceed with the other command to finalize the setup.'
+echo 'Dependencies installation is complete! '
 echo 'Thanks for waiting!'
 lineaSeparador
