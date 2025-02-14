18ace2a4b1c15b7372c66198ad38dd56319cd78c
mra3d <htc.medhat@gmail.com>
2025-02-15 00:18:32 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -1,274 +1,115 @@
-#!/bin/bash
-function lineaSeparador()
-{
-	printf '%.s─' $(seq 1 $(tput cols))
-} 
-function clearConTexto()
-{
-	lineaSeparador
-	echo '[LegitUnlocks]'
-	lineaSeparador
-} 
-function linux_depends()
-{
-	lineaSeparador
-	echo '[Linux]'
-	lineaSeparador
-	echo 'The installation for Linux dependencies needs sudo permission, please input the password of your user (it is invisible) and press return'
-	lineaSeparador
-	echo 'La instalación para las dependencias de Linux necesita permiso sudo, por favor ingresa la contraseña de tu usuario (es invisible) y presiona enter'
-	lineaSeparador	
-	sudo -v
-	if [[ $(which apt-get) ]]
-	then	
-		clearConTexto
-		sudo apt-get install -y \
-			build-essential \
-			curl \
-			checkinstall \
-			git \
-			autoconf \
-			automake \
-			libtool-bin \
-			pkg-config \
-			usbmuxd \
-			libssl-dev \
-			libusb-1.0-0-dev \
-			zlib1g-dev \
-			udev \
-			libreadline-dev \
-			libzip-dev \
-			libxml2-dev \
-			libcurl4-openssl-dev \
-			zlib1g-dev \
-			libusb-dev \
-			libdbus-1-dev \
-			plocate \
-			libglib2.0-dev 
-		clearConTexto
-		sudo apt-get install -y protobuf-compiler
-		clearConTexto
-		sudo apt-get install -y python2 python3 python3-pip python3-tk
-		clearConTexto
-		python3 -m pip install --upgrade pip
-		clearConTexto
-		# Comprobación para libprotobuf.so.23
-		if ! locate libprotobuf.so.23; then
-		    echo "libprotobuf.so.23 no está presente, descargando e instalando..."
-		    # Descargar el paquete .deb de libprotobuf23 desde Debian
-		    wget http://archive.ubuntu.com/ubuntu/pool/main/p/protobuf/libprotobuf23_3.12.4-1ubuntu7_amd64.deb
-		    if [[ -f "libprotobuf23_3.12.4-1ubuntu7_amd64.deb" ]]; then
-		        echo "Paquete libprotobuf23_3.12.4-1ubuntu7_amd64.deb descargado exitosamente."
-		        # Instalar el paquete .deb
-		        sudo dpkg -i libprotobuf23_3.12.4-1ubuntu7_amd64.deb
-		        # Corregir dependencias faltantes si es necesario
-		        sudo apt --fix-broken install
-		        rm libprotobuf23_3.12.4-1ubuntu7_amd64.deb
-		    else
-		        echo "Error: No se pudo descargar el paquete .deb de libprotobuf23."
-		        exit 1
-		    fi
-		else
-		    echo "libprotobuf.so.23 ya existe en el sistema."
-		fi
-		#Solo instalar ideviceactivation si ideviceactivation no está presente
-		if ! ideviceactivation -v ; then
-			build_libimobiledevice
-		else
-			clearConTexto
-			echo '[iDevice]'
-			lineaSeparador
-			echo 'iDevice is already installed. Ok!'
-			lineaSeparador
-			echo 'iDevice ya está instalado. ¡Ok!'
-			lineaSeparador
-			sleep 2
-		fi
-	else
-		clearConTexto
-		echo "ERROR: PLEASE USE A DISTRO USING APT-GET SUCH AS UBUNTU"
-		lineaSeparador
-		echo "ERROR: USA UNA DISTRO QUE TENGA APT-GET COMO UBUNTU"
-		lineaSeparador
-		exit 1
-	fi
-}
-function macos_depends()
-{
-	lineaSeparador
-	echo '[MacOS]'
-	lineaSeparador
-	echo 'The installation for MacOS dependencies needs sudo permission, please input the password of your user (it is invisible) and press return'
-	lineaSeparador
-	echo 'La instalación para las dependencias de MacOS necesita permiso sudo, por favor ingresa la contraseña de tu usuario (es invisible) y presiona enter'
-	lineaSeparador	
-	sudo -v
-	while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-	if [[ ! -e $(which brew) ]] 
-	then
-		clearConTexto
-		echo '[Brew]'
-		lineaSeparador
-		echo 'The package manager brew will be installed.'
-		echo 'Please pay attention if the installation asks to press return (enter)'
-		lineaSeparador
-		echo 'Se instalará el administrador de paquetes brew.'
-		echo 'Presta atención si la instalación pide que presiones return (enter)'
-		lineaSeparador
-		sleep 5
-		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-	else
-		clearConTexto
-		echo '[Brew]'
-		lineaSeparador
-		echo 'brew is already installed. Ok!'
-		lineaSeparador
-		echo 'brew ya está instalado. ¡Ok!'
-		lineaSeparador
-		sleep 2
-	fi
-	clearConTexto
-	echo 'From this point the installation is automated and it may take 10-40 minutes depending on internet and CPU speed.'
-	echo 'So, grab a drink, sit back, relax and please wait.'
-	echo "You'll be unlocking devices very soon <3"
-	lineaSeparador
-	
-	sleep 5
-	clearConTexto
-	#brew doctor
-	#brew cleanup
-	#clearConTexto
-	#echo "Ignore the errors shown here..."
-	#lineaSeparador
-	#echo "Ignora los errores que se muestren..."
-	#lineaSeparador
-	
-	for i in {1..10}; do
-	    sudo chown -R $(whoami) /usr/local/share/man/man$i
-	done
-	
-	brew install pkg-config libtool automake cmake
-	# cmake puede que se instale como x86, elimina todo rastro de cmake y luego ejecuta 
-	# arch -arm64 /opt/homebrew/bin/brew install cmake
-	
-	#rm ~/.zprofile
-	#brew uninstall python-tk
-	#brew uninstall python-tk@3.9
-	#brew uninstall python-tk@3.10
-	#brew uninstall python
-	#brew uninstall python3
-	#brew uninstall python@3.7
-	#brew uninstall python@3.8
-	#brew uninstall python@3.9
-	#brew uninstall python@3.10
-	#clearConTexto
-	#brew install python@3.10
-	#brew link --force --overwrite python@3.10	
-	#brew install python-tk@3.10
-	#brew link --force --overwrite python-tk@3.10
-	clearConTexto
+#!/usr/bin/env bash
 
-	#Solo instalar ideviceactivation si ideviceactivation no está presente
-	if ! ideviceactivation -v ; then
-	
-		#	# Primero Openssl
-		#	
-		#	rm -rf openssl
-		#	git clone https://github.com/openssl/openssl.git
-		#	cd openssl
-		#	./config
-		#	make && sudo make install
-		#	cd ..
-		#	rm -rf openssl
-		#
-		#	# Despues libusb
-		#	
-		#	(obtener zip)
-		#	./configure
-		#	make
-		#	sudo make install
-		#	
-		#	# Despues libzip
-		#	
-		#	(obtener zip)
-		#	mkdir build
-		#	cd build
-		#	cmake ..
-		#	make
-		#	sudo make install
-		#	cd ..
-		
-		build_libimobiledevice
-	else
-		clearConTexto
-		echo '[iDevice]'
-		lineaSeparador
-		echo 'iDevice is already installed. Ok!'
-		lineaSeparador
-		echo 'iDevice ya está instalado. ¡Ok!'
-		lineaSeparador
-		sleep 2
-	fi
-	
-	# Instalar hasta el ultimo python por que instala openssl de brew
-	# Y rompe al compilado.
-	brew install python@3.13 python-tk@3.13
-	
-}
-function build_libimobiledevice()
-{
-	libs=( "libplist" "libtatsu" "libimobiledevice-glue" "libusbmuxd" "libimobiledevice" "usbmuxd" "libirecovery" "ideviceinstaller" "libideviceactivation" "idevicerestore" )
-	buildlibs(){
-		for i in "${libs[@]}"
-		do
-			clearConTexto
-			rm -rf $i
-			echo -e "[IDEV DEP] Fetching $i..."
-			lineaSeparador
-			git clone https://github.com/libimobiledevice/${i}.git
-			cd $i
-			lineaSeparador
-			echo -e "[IDEV DEP] Configuring $i..."
-			lineaSeparador
-			PKG_CONFIG_PATH=/usr/local/lib/pkgconfig ./autogen.sh --without-cython
-			lineaSeparador
-			echo -e "[IDEV DEP] Building $i..."
-			lineaSeparador
-			make
-			lineaSeparador
-			echo -e "[IDEV DEP] Installing $i..."
-			lineaSeparador
-			sudo make install
-			lineaSeparador
-			cd ..
-			rm -rf $i
-			if [[ -e $(which ldconfig) ]] 
-			then
-				sudo ldconfig
-			else 
-				echo ""
-			fi
-		done
-	}
-	buildlibs
-	if [[ -e $(which ldconfig) ]] 
-	then
-		sudo ldconfig
-	else 
-		echo ""
-	fi
-}
-clearConTexto
-echo 'Welcome! This script will install all the dependencies needed'
-lineaSeparador
-if [[ $(uname) == 'Linux' ]]
-then
-	linux_depends
-elif [[ $(uname) == 'Darwin' ]]
-then
-	macos_depends
+export PATH=/usr/local/bin:$PATH
+which brew > /dev/null
+if [ $? -ne 0 ]; then
+    echo "NO HOMEBREW. Install it from brew.sh"
+    exit
+    return
 fi
-clearConTexto
-echo 'Dependencies installation is complete! '
-echo 'Thanks for waiting!'
-lineaSeparador
+
+echo  need full access to install the dependencies
+echo Enter your Mac login password:
+sudo -v
+
+sudo rm -rf dependencies
+sudo mkdir dependencies
+cd dependencies
+sudo mkdir libimobiledevice
+cd libimobiledevice
+
+xcode-select --install
+
+echo " "
+echo Install the Command Line Tools if prompted.
+echo If you see an xcode error ignore it.
+echo " "
+echo :::: Dependencies for MacOS Mojave, Catalina, Big Sur, Monterey ::::
+
+echo " "
+echo NOTE: The code scrolls by very fast. Thats normal.
+echo You may see odd errors and warnings. Thats normal.
+echo DO NOT intervene. Let it run until completion.
+echo " "
+
+read -p "Click Enter to Acknowledge and Continue..."
+
+brew install libusb
+brew install libtool
+brew install automake
+brew install curl
+brew reinstall libxml2
+echo 'export PATH="/usr/local/opt/libxml2/bin:$PATH"' >> ~/.zshrc
+export LDFLAGS="-L/usr/local/opt/libxml2/lib"
+export CPPFLAGS="-I/usr/local/opt/libxml2/include"
+export PKG_CONFIG_PATH="/usr/local/opt/libxml2/lib/pkgconfig"
+brew install gnutls
+brew install libgcrypt
+brew install pkg-config
+brew link pkg-config
+
+echo "[*]Installing openssl ..."
+sudo rm -r -f openssl
+sudo git clone https://github.com/openssl/openssl.git
+cd openssl
+sudo ./config
+sudo make
+sudo make install
+cd ..
+
+echo "[*]Installing libplist ..."
+sudo rm -r -f libplist
+sudo git clone https://github.com/libimobiledevice/libplist.git
+cd libplist
+sudo ./autogen.sh --without-cython
+sudo make
+sudo make install
+cd ..
+
+echo "[*]Installing libimobiledevice-glue ..."
+sudo rm -r -f libimobiledevice-glue
+sudo git clone https://github.com/libimobiledevice/libimobiledevice-glue.git
+cd libimobiledevice-glue
+sudo ./autogen.sh
+sudo make
+sudo make install
+cd ..
+
+echo "[*]Installing libusbmuxd ..."
+sudo rm -r -f libusbmuxd
+sudo git clone https://github.com/libimobiledevice/libusbmuxd.git
+cd libusbmuxd
+sudo ./autogen.sh
+sudo make
+sudo make install
+cd ..
+
+echo "[*]Installing libimobiledevice ..."
+sudo rm -r -f libimobiledevice
+sudo git clone https://github.com/libimobiledevice/libimobiledevice.git
+cd libimobiledevice
+sudo ./autogen.sh --without-cython --disable-openssl
+sudo make
+sudo make install
+cd ..
+
+echo "[*]Installing patched libideviceactivation by OliTheRepairDude ... "
+sudo rm -r -f libideviceactivation
+sudo git clone https://github.com/OliTheRepairDude/libideviceactivation.git
+cd libideviceactivation
+sudo ./autogen.sh
+sudo make 
+sudo make clean
+sudo make install
+cd ..
+
+echo "[*]Installing libirecovery ... "
+sudo rm -r -f libirecovery
+sudo git clone https://github.com/libimobiledevice/libirecovery.git
+cd libirecovery
+sudo ./autogen.sh
+sudo make
+sudo make install
+cd ..
+
+echo "ALL DONE. Everything should be working now."
